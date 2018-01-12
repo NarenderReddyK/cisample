41712be3f17fc4926ed7123947b23f7dcaddf829
Aditya <aditya.enthu@gmail.com>
2018-01-12 12:43:22 +0530

    Updated scripts.

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+
+mvn -DskipTests -P prepare-for-upload package
 appcenter test run appium --app "con_adityadasgupta/cisample" --devices "con_adityadasgupta/firstdeviceset" --app-path ./app/build/outputs/apk/app-debug.apk  --test-series "dev" --locale "en_US" --build-dir target/upload -token 02085aad4964470fac15e75cb5324e3304f762f6
