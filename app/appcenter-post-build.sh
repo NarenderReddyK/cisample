b1b32f300e55d26d9b046a7be89719623fceecb3
Aditya <aditya.enthu@gmail.com>
2018-01-12 17:27:07 +0530

    Configured gradle to publish to play store.

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-cd ../CITest
-mvn -DskipTests -P prepare-for-upload package
-appcenter test run appium --app "con_adityadasgupta/cisample" --devices "con_adityadasgupta/firstdeviceset" --app-path ../app/build/outputs/apk/app-debug.apk  --test-series "dev" --locale "en_US" --build-dir target/upload -token 02085aad4964470fac15e75cb5324e3304f762f6
+if [ "$APPCENTER_BRANCH" == "master" ];
+then
+    ./gradlew publishApkRelease
+else
+    cd ../CITest
+	mvn -DskipTests -P prepare-for-upload package
+	appcenter test run appium --app "con_adityadasgupta/cisample" --devices "con_adityadasgupta/firstdeviceset" --app-path ../app/build/outputs/apk/app-debug.apk  --test-series "dev" --locale "en_US" --build-dir target/upload -token 02085aad4964470fac15e75cb5324e3304f762f6
+fi
