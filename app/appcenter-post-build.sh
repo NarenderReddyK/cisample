be0d54be1b1ca560219777fbbeba49184788dca3
NarenderReddyK <narender78912@gmail.com>
2018-01-16 16:37:06 +0530

    Update appcenter-post-build.sh

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -9,5 +9,5 @@
 # else
 # 	cd ../CITest
 # 	mvn -DskipTests -P prepare-for-upload package
-# 	appcenter test run appium --app "con_adityadasgupta/cisample" --devices "con_adityadasgupta/firstdeviceset" --app-path ../app/build/outputs/apk/app-debug.apk  --test-series "dev" --locale "en_US" --build-dir target/upload --token 02085aad4964470fac15e75cb5324e3304f762f6
+# 	appcenter test run appium --app "con_adityadasgupta/cisample" --devices "con_adityadasgupta/firstdeviceset" --app-path ../app/build/outputs/apk/app-release.apk  --test-series "dev" --locale "en_US" --build-dir target/upload --token 02085aad4964470fac15e75cb5324e3304f762f6
 # fi
